6ed3e62a109758f0c0c7f23759bdca302251d385
Dayalkar2002 <dayalkarmanish123@gmail.com>
2024-02-27 10:07:10 +0530

    update

--- cricket/app/views.py
+++ cricket/app/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.views import View
+from . models import Customer,Cart,Product,OrderPlaced
 # Create your views here.
-def home(request):
- return render(request, 'app/home.html')
 
-def product_detail(request):
- return render(request, 'app/productdetail.html')
+# def home(request):
+#  return render(request, 'app/home.html')
+
+class ProductView(View):
+ def get(self,request):
+  kashmir_willow=Product.objects.filter(category='KW')
+  english_willow=Product.objects.filter(category='EW')
+  return render(request,'app/home.html',{'kashmirwillow':kashmir_willow,'englishwillow':english_willow})
+ 
+ 
+# def product_detail(request):
+#  return render(request, 'app/productdetail.html')
+ 
+class ProductDetailView(View):
+  def get(self,request,id):
+   product=Product.objects.get(pk=id)
+   return render(request,'app/productdetail.html',{'product':product})
 
 def add_to_cart(request):
  return render(request, 'app/addtocart.html')
@@ -25,8 +39,18 @@ def orders(request):
 def change_password(request):
  return render(request, 'app/changepassword.html')
 
-def mobile(request):
- return render(request, 'app/mobile.html')
+def types(request, data=None):
+ if data==None:
+  types=Product.objects.filter(category='EW')
+ elif data=='English':
+  types=Product.objects.filter(category='EW') 
+ elif data=='Kashmir':
+  types=Product.objects.filter(category='KW')
+ elif data=='below':
+  types=Product.objects.all().filter(discounted_price__lt=20000) 
+ elif data=='above':
+  types=Product.objects.all().filter(discounted_price__gt=20000) 
+ return render(request, 'app/types.html',{'types':types})
 
 def login(request):
  return render(request, 'app/login.html')
