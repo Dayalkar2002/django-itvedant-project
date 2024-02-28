7de97ebaa292946c5d0fe74fc7485e2d050aaa45
Dayalkar2002 <dayalkarmanish123@gmail.com>
2024-02-28 13:41:27 +0530

    update

--- cricket/app/views.py
+++ cricket/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.views import View
 from . models import Customer,Cart,Product,OrderPlaced
-from .forms import CustomerRegistrationForm
+from .forms import CustomerRegistrationForm,CustomerProfileForm
 from django.contrib import messages
 from django.contrib.auth import logout
 # Create your views here.
@@ -30,11 +30,11 @@ def add_to_cart(request):
 def buy_now(request):
  return render(request, 'app/buynow.html')
 
-def profile(request):
- return render(request, 'app/profile.html')
+
 
 def address(request):
- return render(request, 'app/address.html')
+ add = Customer.objects.filter(user=request.user)
+ return render(request, 'app/address.html', {'add':add, 'active':'btn-primary',})
 
 def orders(request):
  return render(request, 'app/orders.html')
@@ -76,3 +76,22 @@ def checkout(request):
 def log_out(request):
  logout(request)
  return redirect('/accounts/login/')
+
+class ProfileView(View):
+	def get(self, request):
+		form = CustomerProfileForm()
+		return render(request, 'app/profile.html', {'form':form, 'active':'btn-primary'})
+		
+	def post(self, request):
+		form = CustomerProfileForm(request.POST)
+		if form.is_valid():
+			usr = request.user
+			name  = form.cleaned_data['name']
+			locality = form.cleaned_data['locality']
+			city = form.cleaned_data['city']
+			state = form.cleaned_data['state']
+			zipcode = form.cleaned_data['zipcode']
+			reg = Customer(user=usr, name=name, locality=locality, city=city, state=state, zipcode=zipcode)
+			reg.save()
+			messages.success(request, 'Congratulations!! Profile Updated Successfully.')
+		return render(request, 'app/profile.html', {'form':form, 'active':'btn-primary'})
